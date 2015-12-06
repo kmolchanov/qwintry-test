5809350ecc098f84a46c865763ae3c60dfdf33db
Konstantin Molchanov <kostya.molchanov@gmail.com>
2015-12-06 16:15:57 +0300

    Basic application

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=qwintry-test',
+    'username' => 'qwintry-test',
+    'password' => '',
+    'charset' => 'utf8',
+];
